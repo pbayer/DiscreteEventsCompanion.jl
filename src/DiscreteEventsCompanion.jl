f2e70d62d3b7de35caf17dfb416f79645410073e
Paul Bayer <Paul.Bayer@gleichsam.de>
2020-03-01 10:26:05 +0100

    Files generated by PkgTemplates

---
+++ src/DiscreteEventsCompanion.jl
@@ -0,0 +1,5 @@
+module DiscreteEventsCompanion
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DiscreteEventsCompanion
+using Test
+
+@testset "DiscreteEventsCompanion.jl" begin
+    # Write your own tests here.
+end
